9acf61e5368d7099f896a9c7721b03d9cbd7f759
hyemin-mun <hyemin.mun@illuminarean.com>
2023-05-10 21:46:14 +0900

    BLOG-29 코드리뷰 반영

--- src/layout/TopNavigation.tsx
+++ src/layout/TopNavigation.tsx
@@ -4,6 +4,7 @@ import { centerRowStyles, normalRowStyles } from '@/styles/flexModules';
 import { INaviMenuItem } from '@/types/interfaces/commons';
 import styled from '@emotion/styled';
 import _ from 'lodash';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function TopNavigation() {
@@ -18,22 +19,15 @@ export default function TopNavigation() {
     return (
         <StyledTopNavigation>
             <ul>
-                <StyledNavItem
-                    isSelected={url === MAIN_PAGE_URL}
-                    onClick={onClickNavMenu(MAIN_PAGE_URL)}
-                >
-                    MAIN
+                <StyledNavItem isSelected={url === MAIN_PAGE_URL}>
+                    <Link href={MAIN_PAGE_URL}>MAIN</Link>
                 </StyledNavItem>
                 {_.map(navigationMenu, (item: INaviMenuItem) => {
                     const isSelected = url.startsWith(item.path);
 
                     return (
-                        <StyledNavItem
-                            key={item.id}
-                            isSelected={isSelected}
-                            onClick={onClickNavMenu(item.path)}
-                        >
-                            {item.name}
+                        <StyledNavItem key={item.id} isSelected={isSelected}>
+                            <Link href={item.path}>{item.name}</Link>
                         </StyledNavItem>
                     );
                 })}
@@ -60,17 +54,20 @@ const StyledNavItem = styled.li<{ isSelected?: boolean }>`
     width: 300px;
     height: 100%;
     border-radius: 5px;
-    color: ${(props) =>
-        props.isSelected
-            ? props.theme.colors.point_orange
-            : props.theme.colors.point_yellow_green2};
-
     font-size: 16px;
     font-weight: 900;
     line-height: 42px;
     text-align: center;
 
     cursor: pointer;
+    > a {
+        text-decoration-line: none;
+        text-decoration: none;
+        color: ${(props) =>
+            props.isSelected
+                ? props.theme.colors.point_orange
+                : props.theme.colors.point_yellow_green2};
+    }
 
     :hover {
         box-shadow: 0px 0px 5px #c1cfc1;
