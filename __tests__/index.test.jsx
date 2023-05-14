98a63ab6cbfacb36a8d706f522ea0f9811f03c42
nostrss <jintagi@gmail.com>
2023-05-14 17:33:07 +0900

    add test code

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,12 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
-import Components from '../src/pages/Components';
+import Input from '../src/components/commons/Input';
 
-test('components 페이지', () => {
-    render(<Components />);
-    const heading = screen.getByRole('heading', {
-        name: '컴포넌트 테스트 페이지',
+test('Input 컴포넌트 label 텍스트가 정상적으로 노출되는가?', () => {
+    render(<Input id="test">테스트코드</Input>);
+    const inputNode = screen.getByLabelText('테스트코드', {
+        selector: 'input',
     });
-    expect(heading).toBeInTheDocument();
+    expect(inputNode).toBeInTheDocument();
+});
+
+test('Input 컴포넌트 Placeholder가 정상적으로 나오는가?', () => {
+    render(<Input placeholder="플레이스홀더">테스트코드</Input>);
+    const inputNode = screen.getByPlaceholderText('플레이스홀더');
+    expect(inputNode).toBeInTheDocument();
+});
+
+test('Input 컴포넌트 isSearch === true일 때 아이콘이 노출 되는가', () => {
+    const { getByTestId } = render(<Input isSearch={true} />);
+    const searchIcon = getByTestId('search-icon');
+    expect(searchIcon).toBeInTheDocument();
 });
