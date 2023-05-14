32552957494b9602dd5b9def0e76496be1010704
nostrss <jintagi@gmail.com>
2023-05-14 22:51:57 +0900

    BLOG-32

--- src/types/interfaces/commons.ts
+++ src/types/interfaces/commons.ts
@@ -18,3 +18,31 @@ export interface INaviMenuItem {
     name: string;
     path: string;
 }
+
+export interface IPropsButton {
+    children?: React.ReactNode;
+    disabled?: boolean;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    type?: 'button' | 'submit' | 'reset';
+    href?: string | undefined;
+    style?: {
+        width?: string;
+        height?: string;
+        fontSize?: string;
+        color?: string;
+    };
+}
+
+export interface IPropsStyledButton {
+    buttonStyle: {
+        normal: string;
+        hover: string;
+        style?: {
+            width?: string;
+            height?: string;
+            fontSize?: string;
+            color?: string;
+        };
+    };
+    disabled?: boolean;
+}
