a6cde19ef5daf83fdddb7174b2f003aa7395cca6
Joonyoung <aimzero9303@gmail.com>
2023-05-29 23:22:04 +0900

    BLOG-73 <이미지업로드>

--- src/components/commons/EditorWrite.tsx
+++ src/components/commons/EditorWrite.tsx
@@ -2,8 +2,10 @@ import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { useEffect, useRef } from 'react';
 import { type EditorOptions } from '@/types/interfaces/commons';
-import axios from 'axios';
+
 import { HookCallback } from '@toast-ui/editor/types/editor';
+import axios from '../../api/middlewares';
+import { AxiosResponse } from 'axios';
 
 export default function EditorWrite(options: EditorOptions) {
     const { height = '400px', type, initialValue = ' ', onChange } = options;
@@ -14,18 +16,14 @@ export default function EditorWrite(options: EditorOptions) {
         formData.append('file', file);
         console.log('이미지 파일 정보', file);
         console.log('폼데이터', formData);
-        axios({
+        await axios({
             method: 'post',
             url: 'https://api.blograss.com/image',
             data: formData,
-            headers: {
-                Authorization:
-                    'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtNzFrckBuYXZlci5jb20iLCJleHAiOjE2ODUzNzEyODksInR5cGUiOiJBY2Nlc3MiLCJhdXRob3JpdHkiOiJST0xFX1VTRVIifQ.kQ7Fk046mg0mPiaI3qh8Cgg-OJkMJDN4iaLMOoBqJEdTlGKvZ_AhRvFPcrMYFUJiBj8vsIXfL5ITHpqr3FwOQg',
-            },
         })
-            .then((res) => {
+            .then((res: any) => {
                 console.log(res);
-                const result = `https://blograss-bucket.s3.ap-northeast-2.amazonaws.com/images/${res.data.result[0]}`;
+                const result = `https://blograss-bucket.s3.ap-northeast-2.amazonaws.com/images/${res.result[0]}`;
                 callback(result, 'alt_text');
             })
             .catch((error) => {
